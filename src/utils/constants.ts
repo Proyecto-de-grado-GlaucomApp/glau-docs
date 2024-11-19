012c4c866f0aff0b27bc5f6c18ce16bd34eaed46
FabioB8a <fabiobuitra@gmail.com>
2024-11-18 20:18:53 -0500

    ADD: IP

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,5 @@
 // URL base del API
-export const BASE_URL = "http://localhost:8000/";
+export const BASE_URL = "http://3.211.80.218:8000/";
 
 /**
  * Enum que representa las diferentes acciones disponibles para la autenticación de usuarios.
